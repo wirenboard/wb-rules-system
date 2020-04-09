5f6e75bb6d8efe57dc7d92f316b72230d9f01fe9
munrexio <munrexio@gmail.com>
2020-04-09 20:42:23 +0300

    add wbmz2-battery module

---
+++ rules/wbmz2-battery.js
@@ -0,0 +1,144 @@
+var configPath = "/etc/wbmz2-battery.conf";
+var rcg_ohm = 0.025; // Gas gauge sense resistor (Ω) (10 to 50 mΩ)
+var updateIntervalMs = 3000; //Интервал обновления данных (мс)
+var startChargeCorrection = 0; // Калибровочное значение по умолчанию
+var config = readConfig(configPath);
+var inited = false;
+var wbmz2_ps = new PersistentStorage("wbmz2-battery", {global: true}); // Глобальное хранилище для калибровочных значений
+
+function initDevice() {
+  /*
+  Инициализация модуля 
+  */
+  runShellCommand("i2cset -y {} 0x70 0x00 0x10".format(config.bus));
+
+  defineVirtualDevice("wbmz2-battery", {
+    title: "WBMZ2 Battery",
+    cells: {
+      current: { 
+        type: "current",  
+        value: 0    
+      },
+      voltage: { 
+        type: "voltage",  
+        value: 0    
+      },
+      charge: { 
+        type: "value",  
+        value: 0    
+      },
+      temperature: { 
+        type: "temperature",  
+        value: 0    
+      }
+    }
+  });
+
+  /*
+  Если в хранилище wbmz2_ps.correction пусто, то инициализируем со значением по умолчанию.
+  */
+  if(!wbmz2_ps.correction){
+    wbmz2_ps.correction = startChargeCorrection;
+  }
+
+  inited = true;
+}
+
+function parse2ndComplement(raw) {
+  if (raw > 0x2000) {
+    return raw - 0x4000;
+  } else {
+    return raw;
+  }
+}
+  
+function parse2ndComplement16(raw) {
+  if (raw > 0x8000) {
+    return raw - 0x10000;
+  } else {
+    return raw;
+  }
+}
+  
+function readI2cData() {
+  /*
+  Читаем данные
+  */
+  runShellCommand("i2cdump -y {} 0x70 s | grep 00: | sed -e 's/00: //g' -e 's/    .*//g'".format(config.bus), {
+    captureOutput: true,
+    exitCallback: function (exitCode, capturedOutput) {
+      if(!capturedOutput){
+        /*
+        Записываем в /meta/error значение "r" в случае ошибки чтения данных
+        */
+        publish("/devices/wbmz2-battery/controls/current/meta/error", "r", 2, true);
+        publish("/devices/wbmz2-battery/controls/voltage/meta/error", "r", 2, true);
+        publish("/devices/wbmz2-battery/controls/temperature/meta/error", "r", 2, true);
+        publish("/devices/wbmz2-battery/controls/charge/meta/error", "r", 2, true);
+      }else{
+        /*
+        Удаляем значение "r" из /meta/error если данные прочитаны 
+        */
+        publish("/devices/wbmz2-battery/controls/current/meta/error", "", 2, true);
+        publish("/devices/wbmz2-battery/controls/voltage/meta/error", "", 2, true);
+        publish("/devices/wbmz2-battery/controls/temperature/meta/error", "", 2, true);
+        publish("/devices/wbmz2-battery/controls/charge/meta/error", "", 2, true);
+
+        /*
+        Массив с полученными данными
+        */
+        var arrayOfData = capturedOutput.split(' ');
+
+        /*
+        Сurrent - ток
+        */
+        var currentRaw = parseInt("0x"+arrayOfData[6]+arrayOfData[5], 16);
+        var voltage_uv = 11.77 * parse2ndComplement(currentRaw); // The battery current is coded in 2’s complement format, and the LSB value is 11.77 uV
+        dev['wbmz2-battery']['current'] = Math.round(voltage_uv * 1E-6 / rcg_ohm * 1000) / 1000;
+
+        /*
+        Voltage - напряжение
+        */
+        var voltageRaw = parseInt("0x"+arrayOfData[8]+arrayOfData[7], 16);
+        var voltage_mv = 2.44 * voltageRaw; // The resolution is 2.44 mV for the battery voltage.
+        dev['wbmz2-battery']['voltage'] = Math.round(voltage_mv * 1E-3 * 1000) / 1000;
+
+        /*
+        Temperature - температура модуля wbmz2-battery
+        */
+        var temperatureRaw = parseInt("0x"+arrayOfData[10]+arrayOfData[9], 16);
+        var temperature = 0.125 * parse2ndComplement(temperatureRaw); // The resolution is 0.125° C for the temperature.
+        dev['wbmz2-battery']['temperature'] = Math.round(temperature * 1000) / 1000;
+
+        /*
+        Charge - Ёмкость батареи в мАч (mAh)
+        */
+        var сhargeRaw = parseInt("0x"+arrayOfData[2]+arrayOfData[1], 16);
+        var charge_uvh = 6.70 * parse2ndComplement16(сhargeRaw); // The charge data is coded in 2’s complement format, and the LSB value is 6.70 uV.h.
+        var charge_mah = charge_uvh * 1E-3 / rcg_ohm;
+        /*
+        Если полученные значения ёмкости меньше 0 и меньше калибровочных значений в хранилище с отрицательным знаком, 
+        то вписываем это значение в хранилище wbmz2_ps.correction без отрицательного знака.
+        */
+        if((charge_mah < 0) && ((-charge_mah) > wbmz2_ps.correction)){
+          wbmz2_ps.correction = (-charge_mah);
+        }
+        dev['wbmz2-battery']['charge'] = Math.round((charge_mah + wbmz2_ps.correction)* 100) / 100;
+      }
+    }
+  });
+};
+
+/*
+Обновление данных
+*/
+function update() {
+    config = readConfig(configPath);
+    if(config.enable){
+      if(!inited){
+        initDevice();
+      }
+    readI2cData();
+    }
+};
+setInterval(update, updateIntervalMs);
