177748c6ffaf1282359d680977eaffe096278b09
Nikolay Korotkiy <sikmir@disroot.org>
2023-01-10 12:36:52 +0300

    network: fix 'Active Connection Name' control, cleanup logs

--- rules/network.js
+++ rules/network.js
@@ -60,13 +60,13 @@ defineVirtualDevice("network", {
 });
 
 function _system_update_ip(name, iface) {
-  runShellCommand('ip -o -4 addr show {} | awk -F \' *|/\' \'{print $4}\''.format(iface), {
+  runShellCommand('ip -o -4 addr show {} 2>/dev/null | awk -F \' *|/\' \'{print $4}\''.format(iface), {
     captureOutput: true,
     exitCallback: function (exitCode, capturedOutput) {
       dev.network[name] = capturedOutput;
     }
   });
-  runShellCommand('ping -q -W1 -c3 -I {} {}'.format(iface, checkAddress), {
+  runShellCommand('ping -q -W1 -c3 -I {} {} 2>/dev/null'.format(iface, checkAddress), {
     captureOutput: false,
     exitCallback: function (exitCode) {
       dev.network[name + ' Online Status'] = exitCode === 0;
@@ -75,19 +75,25 @@ function _system_update_ip(name, iface) {
 };
 
 function _current_active_connection() {
-  runShellCommand('ip route get {} | grep -oP \'dev\\s+\\K[^ ]+\''.format(checkAddress), {
+  runShellCommand('ip route get {} 2>/dev/null | grep -oP \'dev\\s+\\K[^ ]+\''.format(checkAddress), {
     captureOutput: true,
     exitCallback: function (exitCode, capturedOutput) {
-      if (exitCode === 0) {
-        dev.network["Default Interface"] = capturedOutput;
-      }
+      dev.network["Default Interface"] = exitCode === 0 ? capturedOutput : "";
     }
   });
-  runShellCommand('nmcli -t -f NAME c s -a', {
+
+  runShellCommand("nmcli -t -f DEVICE,NAME c s -a 2>/dev/null", {
     captureOutput: true,
     exitCallback: function (exitCode, capturedOutput) {
-      if (exitCode === 0) {
-        dev.network["Active Connection Name"] = capturedOutput;
+      if (exitCode != 0) {
+        dev.network["Active Connection Name"] = "";
+        return;
+      }
+      var lines = capturedOutput.split("\n");
+      for (var i = 0; i < lines.length; i++) {
+        if (lines[i].indexOf(dev.network["Default Interface"] + ":") === 0) {
+          dev.network["Active Connection Name"] = lines[i].split(":")[1];
+        }
       }
     }
   });
