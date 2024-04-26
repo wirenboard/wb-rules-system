be2c0452dcc0286bdf86fcc1ae92e1c61f8a4b93
Гамов Алексей Сергеевич <atomos@mail.ru>
2024-04-26 15:47:31 +1100

    Update wbmz-battery.js
    * Убраны лишние ответвления (функции)
    ! Работа скрипта не тестировалась

--- rules/wbmz-battery.js
+++ rules/wbmz-battery.js
@@ -1,182 +1,138 @@
-var configPath = '/etc/wbmz2-battery.conf';
-var rcg_ohm = 0.025; // Gas gauge sense resistor (Ω) (10 to 50 mΩ)
-var updateIntervalMs = 3000; //Интервал обновления данных (мс)
-var startChargeCorrection = 0; // Калибровочное значение по умолчанию
-var config = readConfig(configPath);
-var inited = false;
-var wbmz2_ps = new PersistentStorage('wbmz2-battery', { global: true }); // Глобальное хранилище для калибровочных значений
-
 /* Power status for Wiren Board 6.x/6.7.x with wbmzX-battery module */
 
-var lastTriggeredCurrent = null;
-var currentHysteresis = 0.01;
-var dischargingThreshold = -0.02;
-
-/*Сброс Калибровочных значений*/
-function reset() {
-  runShellCommand('i2cset -y {} 0x70 0x01 0x02'.format(config.bus));
-  wbmz2_ps.correction = startChargeCorrection;
-  wbmz2_ps.min = 0;
-  wbmz2_ps.max = 0;
-  wbmz2_ps.batteryСapacity = 0;
+var params = {
+	correction: 0,
+	trigger: null,
+	hysteresis: 0.01,
+	threshold: -0.02,
+	rcg_ohm: 0.025 // gas gauge sense resistor (Ω) (10 to 50 mΩ)
 }
-
-function updatePowerStatus(newCurrent) {
-  if (lastTriggeredCurrent != null)
-    if (Math.abs(newCurrent - lastTriggeredCurrent) < currentHysteresis) return;
-
-  var newStatus = newCurrent < dischargingThreshold;
-
-  if (dev['power_status/working on battery'] != newStatus) {
-    dev['power_status/working on battery'] = newStatus;
-    lastTriggeredCurrent = newCurrent;
-  }
-}
-
-function initDevice(resetButon) {
-  /*Инициализация модуля*/
-  runShellCommand('i2cset -y {} 0x70 0x00 0x10'.format(config.bus));
-  var cells = {
-    Percentage: {
-      type: 'value',
-      value: 0,
-    },
-    Charge: {
-      title: { en: 'Charge', ru: 'Заряд' },
-      type: 'value',
-      value: 0,
-    },
-    Current: {
-      title: { en: 'Current', ru: 'Ток' },
-      type: 'value',
-      value: 0,
-    },
-    Voltage: {
-      title: { en: 'Voltage', ru: 'Напряжение' },
-      type: 'voltage',
-      value: 0,
-    },
-    Temperature: {
-      title: { en: 'Temperature', ru: 'Температура' },
-      type: 'temperature',
-      value: 0,
-    },
-  };
-  if (resetButon) {
-    cells['Reset'] = {
-      type: 'pushbutton',
-    };
-  }
-  defineVirtualDevice('battery', {
-    title: { en: 'Battery', ru: 'Батарея' },
-    cells: cells,
-  });
-
-  if (!wbmz2_ps.correction) {
-    reset();
-  }
-}
-
-defineRule('_reset_calib', {
-  whenChanged: ['battery/Reset'],
-  then: function (newValue, devName, cellName) {
-    reset();
-  },
-});
 
 function parse2ndComplement(raw) {
-  if (raw > 0x2000) {
-    return raw - 0x4000;
-  } else {
-    return raw;
-  }
+	return (raw > 0x2000) ? raw - 0x4000 : raw;
 }
 
 function parse2ndComplement16(raw) {
-  if (raw > 0x8000) {
-    return raw - 0x10000;
-  } else {
-    return raw;
-  }
+	return (raw > 0x8000) ? raw - 0x10000 : raw;
 }
+
+/* Функция сброса калибровочных значений */
+
+function reset() {
+	runShellCommand('i2cset -y {} 0x70 0x01 0x02'.format(config.bus));
+	wbmz2_ps.correction = params.correction;
+	wbmz2_ps.min = 0;
+	wbmz2_ps.max = 0;
+	wbmz2_ps.batteryСapacity = 0;
+}
+
+/* Функция читения данных */
 
 function readI2cData() {
-  /*Читаем данные*/
-  runShellCommand(
-    "i2cdump -y -r 0x01-0x0C {} 0x70 c | grep 00: | sed -e 's/00: //g' -e 's/    .*//g'".format(
-      config.bus
-    ),
-    {
-      captureOutput: true,
-      exitCallback: function (exitCode, capturedOutput) {
-        if (!capturedOutput) {
-          /*Записываем в /meta/error значение "r" в случае ошибки чтения данных*/
-          publish('/devices/battery/controls/Current/meta/error', 'r', 2, true);
-          publish('/devices/battery/controls/Voltage/meta/error', 'r', 2, true);
-          publish('/devices/battery/controls/Temperature/meta/error', 'r', 2, true);
-          publish('/devices/battery/controls/Charge/meta/error', 'r', 2, true);
-          publish('/devices/battery/controls/Percentage/meta/error', 'r', 2, true);
-        } else {
-          /*Удаляем значение "r" из /meta/error если данные прочитаны */
-          publish('/devices/battery/controls/Current/meta/error', '', 2, true);
-          publish('/devices/battery/controls/Voltage/meta/error', '', 2, true);
-          publish('/devices/battery/controls/Temperature/meta/error', '', 2, true);
-          publish('/devices/battery/controls/Charge/meta/error', '', 2, true);
-          publish('/devices/battery/controls/Percentage/meta/error', '', 2, true);
+	runShellCommand("i2cdump -y -r 0x01-0x0C {} 0x70 c | grep 00: | sed -e 's/00: //g' -e 's/    .*//g'".format(config.bus), {
+		captureOutput: true,
+		exitCallback: function (code, output) {
+			// Записываем в /meta/error значение "r" в случае ошибки чтения данных и удаляем его если данные успешно прочитаны
+			['Current', 'Voltage', 'Temperature', 'Charge', 'Percentage'].forEach(function(ctrl) {
+				publish('/devices/battery/controls/{}/meta/error'.format(ctrl), (!output ? 'r' : ''), 2, true);
+			});
 
-          /*Массив с полученными данными*/
-          var arrayOfData = capturedOutput.trim().split(' ');
+			// Оставливаем дальнейший код если нет выходных данных
+			if (!output) return; 
 
-          /*Сurrent*/
-          var currentRaw = parseInt('0x' + arrayOfData[6] + arrayOfData[5], 16);
-          var voltage_uv = 11.77 * parse2ndComplement(currentRaw); // The battery current is coded in 2’s complement format, and the LSB value is 11.77 uV
-          var current = Math.round(((voltage_uv * 1e-6) / rcg_ohm) * 1000) / 1000;
-          dev['battery']['Current'] = current;
-          updatePowerStatus(current);
+			// Массив с полученными данными
+			var data = output.trim().split(' ');
 
-          /*Voltage*/
-          var voltageRaw = parseInt('0x' + arrayOfData[8] + arrayOfData[7], 16);
-          var voltage_mv = 2.44 * voltageRaw; // The resolution is 2.44 mV for the battery voltage.
-          dev['battery']['Voltage'] = Math.round(voltage_mv * 1e-3 * 1000) / 1000;
+			// Вычисление силы тока
+			var currentRaw = parseInt('0x' + data[6] + data[5], 16);
+			var voltage_uv = 11.77 * parse2ndComplement(currentRaw); // The battery current is coded in 2’s complement format, and the LSB value is 11.77 uV
+			var current = Math.round(((voltage_uv * 1e-6) / params.rcg_ohm) * 1000) / 1000;
 
-          /*Temperature*/
-          var temperatureRaw = parseInt('0x' + arrayOfData[10] + arrayOfData[9], 16);
-          var temperature = 0.125 * parse2ndComplement(temperatureRaw); // The resolution is 0.125° C for the temperature.
-          dev['battery']['Temperature'] = Math.round(temperature * 1000) / 1000;
+			dev.battery['Current'] = current;
 
-          /*Charge (mAh)*/
-          var сhargeRaw = parseInt('0x' + arrayOfData[2] + arrayOfData[1], 16);
-          var charge_uvh = 6.7 * parse2ndComplement16(сhargeRaw); // The charge data is coded in 2’s complement format, and the LSB value is 6.70 uV.h.
-          var charge_mah = (charge_uvh * 1e-3) / rcg_ohm;
+			// Вычисление напряжения
+			var voltageRaw = parseInt('0x' + data[8] + data[7], 16);
+			var voltage_mv = 2.44 * voltageRaw; // resolution is 2.44 mV
 
-          if (charge_mah < wbmz2_ps.min) {
-            wbmz2_ps.min = charge_mah;
-          } else if (charge_mah > wbmz2_ps.max) {
-            wbmz2_ps.max = charge_mah;
-          }
-          wbmz2_ps.batteryСapacity = wbmz2_ps.max - wbmz2_ps.min;
+			dev.battery['Voltage'] = Math.round(voltage_mv * 1e-3 * 1000) / 1000;
 
-          if (charge_mah - wbmz2_ps.correction < 0) {
-            wbmz2_ps.correction = charge_mah;
-          } else if (charge_mah - wbmz2_ps.correction > wbmz2_ps.batteryСapacity) {
-            wbmz2_ps.correction = -(wbmz2_ps.batteryСapacity - charge_mah);
-          }
-          dev['battery']['Charge'] = Math.round((charge_mah - wbmz2_ps.correction) * 100) / 100;
-          dev['battery']['Percentage'] = Math.round(
-            (charge_mah - wbmz2_ps.correction) / (wbmz2_ps.batteryСapacity / 100)
-          );
-        }
-      },
-    }
-  );
+			// Вычисление температуры
+			var temperatureRaw = parseInt('0x' + data[10] + data[9], 16);
+			var temperature = 0.125 * parse2ndComplement(temperatureRaw); // resolution is 0.125° C
+
+			dev.battery['Temperature'] = Math.round(temperature * 1000) / 1000;
+
+			// Вычисление заряда (mAh)
+			var сhargeRaw = parseInt('0x' + data[2] + data[1], 16);
+			var charge_uvh = 6.7 * parse2ndComplement16(сhargeRaw); // The charge data is coded in 2’s complement format, and the LSB value is 6.70 uV.h.
+			var charge_mah = charge_uvh * 1e-3 / params.rcg_ohm;
+
+			if (charge_mah < wbmz2_ps.min) {
+				wbmz2_ps.min = charge_mah;
+			}
+			else if (charge_mah > wbmz2_ps.max) {
+				wbmz2_ps.max = charge_mah;
+			}
+
+			wbmz2_ps.batteryСapacity = wbmz2_ps.max - wbmz2_ps.min;
+
+			if (charge_mah - wbmz2_ps.correction < 0) {
+				wbmz2_ps.correction = charge_mah;
+			}
+			else if (charge_mah - wbmz2_ps.correction > wbmz2_ps.batteryСapacity) {
+				wbmz2_ps.correction = -(wbmz2_ps.batteryСapacity - charge_mah);
+			}
+
+			dev.battery['Charge'] = Math.round((charge_mah - wbmz2_ps.correction) * 100) / 100;
+			dev.battery['Percentage'] = Math.round((charge_mah - wbmz2_ps.correction) / (wbmz2_ps.batteryСapacity / 100));
+
+			//
+
+			if (lastTrigger != null && Math.abs(current - lastTrigger) < params.hysteresis) return;
+
+			var state = current < params.threshold;
+
+			if (dev.power_status['working on battery'] != state) {
+				dev.power_status['working on battery'] = state;
+				params.trigger = current;
+			}
+		}
+	});
 }
 
-function update() {
-  readI2cData();
-}
+var wbmz2_ps = new PersistentStorage('wbmz2-battery', {global: true}); // глобальное хранилище для калибровочных значений
 
-config = readConfig(configPath);
+var updateIntervalMs = 3000; // интервал обновления данных (мс)
+var config = readConfig('/etc/wbmz2-battery.conf');
+
 if (config.enable) {
-  initDevice(config.resetButon);
-  setInterval(update, updateIntervalMs);
+	runShellCommand('i2cset -y {} 0x70 0x00 0x10'.format(config.bus));
+
+	var controls = {
+		Percentage: {title: {en: 'Charge left', ru: 'Остаток'}, type: 'value', value: 0},
+		Charge: {title: {en: 'Charge', ru: 'Зарядка'}, type: 'value', value: 0},
+		Current: {title: {en: 'Current', ru: 'Потребление'}, type: 'value', value: 0},
+		Voltage: {title: {en: 'Voltage', ru: 'Напряжение'}, type: 'voltage', value: 0},
+		Temperature: {title: {en: 'Temperature', ru: 'Температура'}, type: 'temperature', value: 0},
+	};
+
+	if (config.resetButon) {
+		controls['Reset'] = {title: {en: 'Reset', ru: 'Сброс'}, type: 'pushbutton'};
+
+		defineRule('_reset_calib', {
+			whenChanged: 'battery/Reset',
+			then: function () {
+				reset();
+			}
+		});
+	}
+
+	if (!wbmz2_ps.correction) reset();
+
+	defineVirtualDevice('battery', {
+		title: {en: 'Battery', ru: 'Батарея'},
+		cells: controls
+	});
+
+	setInterval(readI2cData, updateIntervalMs);
 }
