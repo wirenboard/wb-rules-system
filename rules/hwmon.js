2cdbfe5e652f5f2e8f226d9e343f8a033feed69f
Гамов Алексей Сергеевич <atomos@mail.ru>
2024-04-26 15:36:09 +1100

    Update hwmon.js
    * Общая минимизация

--- rules/hwmon.js
+++ rules/hwmon.js
@@ -1,110 +1,112 @@
 function _str_split_space(s) {
-  var index = s.indexOf(' ');
-  if (index == -1) {
-    return [s];
-  } else {
-    return [s.slice(0, index), s.slice(index + 1)];
-  }
+	var index = s.indexOf(' ');
+	return (index == -1) ? [s] : [s.slice(0, index), s.slice(index + 1)];
 }
 
 var nodeInfo = {};
-runShellCommand(
-  'set /proc/device-tree/wirenboard/hwmon-nodes/*/*; [ -e "$1" ] || shift; for i; do echo -n $i; echo \' \'`cat $i`; done',
-  {
-    captureOutput: true,
-    exitCallback: function (exitCode, capturedOutput) {
-      if (exitCode != 0) return;
-      var strList = capturedOutput.split('\n');
-      for (var i = 0; i < strList.length; ++i) {
-        var strParts = _str_split_space(strList[i]);
-        if (strParts.length == 2) {
-          var path = strParts[0];
-          var contents = strParts[1];
 
-          var match = path.match(/\/([^\/]*)\/([^\/]*)$/);
-          var nodeName = match[1];
-          var propName = match[2];
-          if (!nodeInfo[nodeName]) {
-            nodeInfo[nodeName] = {};
-          }
+runShellCommand('set /proc/device-tree/wirenboard/hwmon-nodes/*/*; [ -e "$1" ] || shift; for i; do echo -n $i; echo \' \'`cat $i`; done', {
+	captureOutput: true,
+	exitCallback: function (code, output) {
+		if (code != 0) return;
 
-          nodeInfo[nodeName][propName] = contents;
-        }
-      }
+		var strList = output.split('\n');
 
-      var cells = {};
-      for (var nodeName in nodeInfo) {
-        if (nodeInfo.hasOwnProperty(nodeName)) {
-          var node = nodeInfo[nodeName];
-          cells[node['title']] = { type: 'temperature', value: 0.0 };
-        }
-      }
+		for (var i = 0; i < strList.length; ++i) {
+			var strParts = _str_split_space(strList[i]);
+			
+			if (strParts.length == 2) {
+				var path = strParts[0];
+				var contents = strParts[1];
 
-      if (Object.keys(cells).length != 0) {
-        defineVirtualDevice('hwmon', {
-          title: { en: 'HW Monitor', ru: 'HW монитор' },
-          cells: cells,
-        });
+				var match = path.match(/\/([^\/]*)\/([^\/]*)$/);
+				var nodeName = match[1];
+				var propName = match[2];
 
-        initHwmonSysfs();
-      }
-    },
-  }
-);
+				if (!nodeInfo[nodeName]) {
+					nodeInfo[nodeName] = {};
+				}
+
+				nodeInfo[nodeName][propName] = contents;
+			}
+		}
+
+		var controls = {};
+
+		for (var nodeName in nodeInfo) {
+			if (nodeInfo.hasOwnProperty(nodeName)) {
+				var node = nodeInfo[nodeName];
+				controls[node['title']] = {type: 'temperature', value: 0.0};
+			}
+		}
+
+		if (Object.keys(controls).length != 0) {
+			defineVirtualDevice('hwmon', {
+				title: {en: 'Hardware monitor', ru: 'Статус оборудования'},
+				cells: controls,
+			});
+
+			initHwmonSysfs();
+		}
+	}
+});
 
 var sysfsMapping = {};
-function initHwmonSysfs() {
-  // map hwmonN sysfs directory and hwmon-nodes channels
-  runShellCommand(
-    "for i in /sys/class/hwmon/hwmon*/name /sys/class/hwmon/hwmon*/of_node/name; do echo -n $i; echo ' '`cat $i`; done",
-    {
-      captureOutput: true,
-      exitCallback: function (exitCode, capturedOutput) {
-        var strList = capturedOutput.split('\n');
-        for (var i = 0; i < strList.length; ++i) {
-          var strParts = _str_split_space(strList[i]);
-          if (strParts.length == 2) {
-            var path = strParts[0];
-            var sysfsDirPath = path.match(/^(\/sys\/class\/hwmon\/hwmon[^\/]+)\//)[1];
-            var nodeName = strParts[1];
-            sysfsMapping[nodeName] = sysfsDirPath;
-          }
-        }
 
-        initReadRules();
-      },
-    }
-  );
+function initHwmonSysfs() {
+	// map hwmonN sysfs directory and hwmon-nodes channels
+
+	runShellCommand("for i in /sys/class/hwmon/hwmon*/name /sys/class/hwmon/hwmon*/of_node/name; do echo -n $i; echo ' '`cat $i`; done", {
+		captureOutput: true,
+		exitCallback: function (code, output) {
+			var strList = output.split('\n');
+
+			for (var i = 0; i < strList.length; ++i) {
+				var strParts = _str_split_space(strList[i]);
+
+				if (strParts.length == 2) {
+					var path = strParts[0];
+					var sysfsDirPath = path.match(/^(\/sys\/class\/hwmon\/hwmon[^\/]+)\//)[1];
+					var nodeName = strParts[1];
+
+					sysfsMapping[nodeName] = sysfsDirPath;
+				}
+			}
+
+			initReadRules();
+		}
+	});
 }
 
 function readChannel(path, controlName) {
-  runShellCommand('cat ' + path, {
-    captureOutput: true,
-    exitCallback: function (exitCode, capturedOutput) {
-      if (exitCode == 0) {
-        dev['hwmon'][controlName] = parseFloat((parseInt(capturedOutput) * 0.001).toFixed(3));
-      }
-    },
-  });
+	runShellCommand('cat ' + path, {
+		captureOutput: true,
+		exitCallback: function (code, output) {
+			if (code) return;
+			dev.hwmon[controlName] = parseFloat((parseInt(output) * 0.001).toFixed(3));
+		}
+	});
 }
 
 function initReadRules() {
-  for (var nodeName in nodeInfo) {
-    if (nodeInfo.hasOwnProperty(nodeName)) {
-      var node = nodeInfo[nodeName];
+	for (var nodeName in nodeInfo) {
+		if (nodeInfo.hasOwnProperty(nodeName)) {
+			var node = nodeInfo[nodeName];
 
-      var sysfsDirPath = sysfsMapping[node['hwmon-node-name']];
-      if (sysfsDirPath) {
-        var path = sysfsDirPath + '/' + node['hwmon-channel'] + '_input';
-        var controlName = node['title'];
+			var sysfsDirPath = sysfsMapping[node['hwmon-node-name']];
 
-        readChannel(path, controlName);
-        (function (path, controlName) {
-          setInterval(function () {
-            readChannel(path, controlName);
-          }, 10000);
-        })(path, controlName);
-      }
-    }
-  }
+			if (sysfsDirPath) {
+				var path = sysfsDirPath + '/' + node['hwmon-channel'] + '_input';
+				var controlName = node['title'];
+
+				readChannel(path, controlName);
+
+				(function (path, controlName) {
+					setInterval(function () {
+						readChannel(path, controlName);
+					}, 10000);
+				})(path, controlName);
+			}
+		}
+	}
 }
