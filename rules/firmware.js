7816c8f9f6e555eb9fea46528a496bc2b4e5fc54
Nikolay Korotkiy <sikmir@disroot.org>
2024-09-23 14:59:38 +0400

    Add firmware.js rule

---
+++ rules/firmware.js
@@ -0,0 +1,32 @@
+function publishFitFileInfo(filePath, presentTopic, compatibilityTopic) {
+  runShellCommand('test -f {}'.format(filePath), {
+    exitCallback: function (exitCode) {
+      if (exitCode != 0) {
+        publish(presentTopic, 'false', 0, true);
+        return;
+      }
+      publish(presentTopic, 'true', 0, true);
+      runShellCommand('FIT={} wb-fw-compatible'.format(filePath), {
+        captureOutput: true,
+        exitCallback: function (exitCode, capturedOutput) {
+          if (exitCode != 0) return;
+          publish(compatibilityTopic, capturedOutput, 0, true);
+        }
+      });
+    }
+  });
+}
+
+publishFitFileInfo(
+  '/mnt/data/.wb-restore/factoryreset.fit',
+  '/firmware/fits/factoryreset/present',
+  '/firmware/fits/factoryreset/compatibility'
+);
+
+publishFitFileInfo(
+  '/mnt/data/.wb-restore/factoryreset.original.fit',
+  '/firmware/fits/factoryreset-original/present',
+  '/firmware/fits/factoryreset-original/compatibility'
+);
+
+publish('/firmware/status', 'IDLE', 0, true);
